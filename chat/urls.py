c9fda744226f88e6e89b4c0177be556312b461fe
Omotayo <omotayo8@live.com>
2022-07-05 05:03:05 +0100

    adding latest changes

--- chat/urls.py
+++ chat/urls.py
@@ -3,19 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('room-link/', views.roomLink, name='room-link'),
+    #path('room-link/', views.roomLink, name='room-link'),
     path('create-room/', views.home, name='home'),
     #path('', views.home, name='home'),
     
     #Inaccessibles
-    path('<str:room>/<int:id>/', views.room, name='room'),
-    path('checkview', views.checkview, name='checkview'),
+    path('<str:room>/', views.room, name='room'),
+    #path('checkview', views.checkview, name='checkview'),
+    path('create-room/checkview', views.checkview, name='checkview'),
     path('send', views.send, name='send'),
     path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
 
 #'/'+room+'/?username='+username
-
-#<url> + randomly generated room id , which will be a alpha numeric value
-#room - id
-#id - randomly generated id
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
+from .dowellconnection import dowellconnection
 #import requests
 
 # Create your views here.
@@ -24,20 +25,35 @@ r=Dowell_Login()
 '''
 
 def home(request):
+    
+    if "session_id" in request.session:
+        dic = request.session
+        session_id = dic["session_id"]
+        field = {"SessionID":session_id}
+        context= {}
+        data = dowellconnection("login","bangalore","login","login","login","6752828281","ABCDE","fetch",field,"nil")
+        data1 = json.loads(data)
+        lstodic = data1["data"][-1]
+        context["username"] = lstodic["Username"]
+        
+        return render(request, 'home.html', context)
+    
+    else:
+        return redirect("https://100014.pythonanywhere.com/")
 
-    return render(request, 'home.html')
+    #return render(request, 'home.html')
 
 # Homepage
 def main(request):
     
     return render(request, 'main.html')
-
-def roomLink(request):
-    
+'''
+def roomLink(request):    
     return render(request, 'room-link.html')
-
+'''
         
-def room(request, room, id):
+#def room(request, room, id):
+def room(request, room):
     username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
     message = Message.objects.all()
@@ -64,7 +80,7 @@ def send(request):
     message = request.POST['message']
     username = request.POST['username']
     room_id = request.POST['room_id']
-
+    
     new_message = Message.objects.create(value=message, user=username, room=room_id)
     new_message.save()
     return HttpResponse('Message sent successfully')
